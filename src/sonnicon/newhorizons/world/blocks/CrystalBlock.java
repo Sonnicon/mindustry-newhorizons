67c6d57fb284f7111e55be8299110e8bc0d68ae4
Sonnicon <sonniconn@gmail.com>
2020-12-08 15:25:04 +0000

    Bouncy bullets

--- src/sonnicon/newhorizons/world/blocks/CrystalBlock.java
+++ src/sonnicon/newhorizons/world/blocks/CrystalBlock.java
@@ -1,12 +1,17 @@
 package sonnicon.newhorizons.world.blocks;
 
+import arc.math.Rand;
 import mindustry.content.Items;
+import mindustry.entities.bullet.BulletType;
 import mindustry.game.Team;
 import mindustry.gen.Building;
 import mindustry.gen.Bullet;
 import mindustry.type.Category;
 import mindustry.world.Block;
+import mindustry.world.Tile;
 import sonnicon.newhorizons.content.Types;
+
+import java.util.Random;
 
 import static mindustry.type.ItemStack.with;
 
@@ -32,7 +37,7 @@ public class CrystalBlock extends Block{
         size = 3;
     }
 
-
+    protected final Random random = new Random();
 
     public class CrystalBlockBuild extends Building{
         public CrystalBlockBuild(){
@@ -40,14 +45,31 @@ public class CrystalBlock extends Block{
         }
 
         @Override
+        public void team(Team team){
+            this.team = Team.derelict;
+        }
+
+        @Override
         public boolean collide(Bullet other){
-            //todo
-            return Types.lasers.contains(other.type());
+            return other.owner() != this;
+        }
+
+        @Override
+        public boolean collision(Bullet other){
+            BulletType type = other.type();
+            if(Types.lasers.contains(other.type())){
+                for(int i = 0; i < 3; i++){
+                    type.create(this, null, x(), y(), random.nextInt(360));
+                }
+            }else{
+                //todo
+            }
+            return true;
         }
 
         @Override
         public void damage(float damage){
-
+            tile.setTeam(Team.derelict);
         }
     }
 }
