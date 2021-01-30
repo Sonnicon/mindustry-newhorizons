83b2d41a4335dd42936bfb7bb6e8591b8d631a76
Sonnicon <sonniconn@gmail.com>
2021-01-30 07:48:50 +0000

    Change how BlockSemiMirror works

--- src/sonnicon/newhorizons/world/blocks/beam/BlockSemiMirror.java
+++ src/sonnicon/newhorizons/world/blocks/beam/BlockSemiMirror.java
@@ -3,6 +3,7 @@ package sonnicon.newhorizons.world.blocks.beam;
 import mindustry.entities.bullet.BulletType;
 import mindustry.gen.Bullet;
 import sonnicon.newhorizons.content.Types;
+import sonnicon.newhorizons.entities.PowerBeam;
 
 public class BlockSemiMirror extends BlockMirror{
 
@@ -13,34 +14,34 @@ public class BlockSemiMirror extends BlockMirror{
     public class BuildingSemiMirror extends BuildingMirror{
         @Override
         public boolean collision(Bullet other){
+            float bRotation = (180f - other.rotation()) % 360f;
             BulletType type = other.type();
             if(Types.isLaser(type)){
-                float time = other.lifetime - (other.lifetime() - other.time()) / 2;
-                int angle = (180 - (int) other.rotation()) % 360;
-
-                if(angle % 180 != setting){
-                    Bullet b = type.create(this, null, other.x(), other.y(), angle - 2 * setting);
-                    b.time(time);
-                }
-
-                if(type.collidesTeam){
-                    other = type.create(this, null, other.x(), other.y(), other.rotation());
-                    other.time(time);
+                if(shouldReflectAngle(bRotation)){
+                    float bounceAngle = bRotation - 2f * config();
+                    if(type.collidesTeam){
+                        Bullet b = type.create(this, null, other.x(), other.y(), bounceAngle);
+                        b.time(other.time());
+                        return true;
+                    }else{
+                        // Don't create new bullets if not required
+                        other.owner(this);
+                        other.team(null);
+                        other.rotation(bounceAngle);
+                        return false;
+                    }
+                }else if(type.collidesTeam){
+                    Bullet b = type.create(this, null, other.x(), other.y(), other.rotation());
+                    b.time(other.time());
                     return true;
-                }else{
-                    // Don't create new bullets if not required
-                    other.time(time);
-                    other.owner(this);
-                    other.team(null);
-                    return false;
                 }
             }
             return super.collision(other);
         }
 
         @Override
-        public boolean shouldReflectAngle(float rotation){
-            return true;
+        public boolean shouldDamage(PowerBeam beam){
+            return false;
         }
     }
 }
